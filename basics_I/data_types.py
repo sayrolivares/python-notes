86a37a3515417aad854d58f74d1188b2991ff232
Enrique Olivares <enrique.sandoval@outlook.de>
2024-02-12 00:04:06 -0500

    first commit

---
+++ basics_I/data_types.py
@@ -0,0 +1,45 @@
+#fundamental data types
+#str
+x = 'Hello world'
+
+#int
+x= 20
+
+#float
+x = 20.5
+
+#complex
+x = 1j
+
+#list
+x = ['apple', 'banana', 'cherry']
+
+#tuple
+x = ('apple', 'banana', 'cherry')
+
+#range
+x = range(6)
+
+#dict
+x = {'name' : 'John', 'age' : 36}
+
+#set
+x = {'apple', 'banana', 'cherry'}
+
+#frozenset
+x = frozenset({'apple', 'banana', 'cherry'})
+
+#bool
+x = True
+
+#bytes
+x = b'hello'
+
+#bytearray
+x= bytearray(5)
+
+#memoryview
+x = memoryview(bytes(5))
+
+#nonetype
+x = None
